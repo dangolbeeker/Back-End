9d1b2061b60654c90f9e8f6b8e3b27eafd312dde
Gurpreet Dhaliwal <gdhaliwal22@gmail.com>
2020-01-08 09:57:59 -0800

    updated property routes in server.js

--- api/server.js
+++ api/server.js
@@ -3,6 +3,7 @@ const configureMiddleware = require("./configurationMiddleware.js");
 const restrictedAuth = require("../auth/restrictedAuthMiddleware.js");
 
 const usersRouter = require("../users/usersRouter.js");
+const propertyRouter = require("../property/propertyRouter.js");
 const authRouter = require("../auth/authRouter.js");
 
 const server = express();
@@ -11,6 +12,7 @@ configureMiddleware(server);
 
 server.use("/api/auth", authRouter);
 server.use("/api/users", restrictedAuth, usersRouter);
+server.use("/api/property", restrictedAuth, propertyRouter);
 
 server.get("/", (req, res) => {
   res.send("Hello, server is running");
